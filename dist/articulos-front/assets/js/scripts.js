761e6fc46f30c9964449408c7fe6d2ffcf3ac7a5
Yamil Martinez <yamil123_1@live.com>
2021-01-14 01:03:09 -0300

    reload dist folder

--- dist/articulos-front/assets/js/scripts.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $(".navbar-burger").on('click', function() {
-
-    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-    $(".navbar-burger").toggleClass("is-active");
-    $(".navbar-menu").toggleClass("is-active");
-
-  });
-})
