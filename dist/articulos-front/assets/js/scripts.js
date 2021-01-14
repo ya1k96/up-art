bad02224ce1e014d4b6d2b04a432cd04624b5a27
Yamil Martinez <yamil123_1@live.com>
2021-01-14 14:08:31 -0300

    dist recargado

---
+++ dist/articulos-front/assets/js/scripts.js
@@ -0,0 +1,9 @@
+$(function() {
+  $(".navbar-burger").on('click', function() {
+
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+
+  });
+})
